068d459388841908f659ab39a6c5402ea0967582
Ferens Dmitriy <ferensdima@gmail.com>
2014-05-14 16:14:44 +0300

    Added tastypie's resources support

--- angularmagic/external.py
+++ angularmagic/external.py
@@ -6,6 +6,10 @@ try:
     import rest_framework
 except ImportError:
     rest_framework = False
+try:
+    import tastypie
+except ImportError:
+    tastypie = False
 
 import django.db.models
 
@@ -26,6 +30,29 @@ if rest_framework:
             return self.SerializerClass(self.obj, many=many).data
 
 
+if tastypie:
+    class TastypieSerializerAdapter(base.Serializer):
+
+        def __init__(self, ResourceClass, obj):
+            super(TastypieSerializerAdapter, self).__init__(obj)
+            self.resource = ResourceClass()
+            self.model = ResourceClass._meta.queryset.model
+            self.obj = obj
+
+        def serialize(self):
+            if hasattr(self.obj, '__iter__'):
+                def serialize_obj(obj):
+                    bundle = self.resource.build_bundle(obj=obj)
+                    self.resource.full_dehydrate(bundle, for_list=True)
+                    return bundle.data
+
+                return [serialize_obj(obj) for obj in self.obj]
+            else:
+                bundle = self.resource.build_bundle(obj=self.obj)
+                self.resource.full_dehydrate(bundle)
+                return bundle.data
+
+
 def create_serializer_adapter(SerializerClass, obj):
     """
     :return: ``angularmagic.base.Serializer`` instance.
@@ -35,6 +62,12 @@ def create_serializer_adapter(SerializerClass, obj):
 
         if issubclass(SerializerClass, Serializer):
             return RestFrameworkSerializerAdapter(SerializerClass, obj)
+
+    if tastypie:
+        from tastypie.resources import Resource
+
+        if issubclass(SerializerClass, Resource):
+            return TastypieSerializerAdapter(SerializerClass, obj)
 
     raise TypeError('Unknown serializer class: %s' % SerializerClass.__name__)
 
