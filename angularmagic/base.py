db80b9dff6d1aaacda474075fbc16ee809e8c452
Ferens Dmitriy <ferensdima@gmail.com>
2014-05-16 21:59:53 +0300

    Added docstrings to base classes

--- angularmagic/base.py
+++ angularmagic/base.py
@@ -1,20 +1,56 @@
 class Serializer(object):
+    """
+    Converts complex objects such as querysets or model instances to
+    native Python datatypes that can be converted to xml/json/anything later.
 
-    model = NotImplemented
+    Is a light wrapper of ``rest_framework.serializers.Serializer``.
+
+    :param model: class if serializer is class-specific.
+    :type model: type
+    """
+
+    model = None
 
     def __init__(self, obj):
+        """
+        Initializes serializer instance.
+
+        :param obj: object to serialize
+        """
         pass
 
     def serialize(self):
+        """
+        Actually serializes previously passed object.
+
+        :return: 
+        """
         raise NotImplementedError
 
 
 class Renderer(object):
+    """
+    Renders passed data to string.
 
-    format = NotImplemented
+    Is a light wrapper of ``rest_framework.renderers.Renderer``.
+
+    :param format:  (like 'json' or 'xml')
+    """
+
+    format = 'unknown'
 
     def __init__(self, data):
+        """
+        Initializes renderer instance.
+
+        :param data: data to render
+        """
         pass
 
     def render(self):
+        """
+        Actually renders previously passed data.
+
+        :return: rendered data as string
+        """
         raise NotImplementedError
